772ed5d26a575bd432bd2155a0f8235f40d60c81
yogi1308 <shreetej.yogi@gmail.com>
2026-03-25 16:25:14 -0700

    added hyprsunset. fixed browser resizing by setting the default to float and making it fullscreen by listening to openwindow using hyprwhenthen and if it is the first zen window it that workspace then full screen it.

--- hyprwhenthen/.config/hyprwhenthen/float.sh
+++ hyprwhenthen/.config/hyprwhenthen/float.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 ADDRESS="0x$1"
+WORKSPACE=$(hyprctl clients -j | jq -r --arg addr "$ADDRESS" '.[] | select(.address == $addr) | .workspace.id')
+COUNT=$(hyprctl clients -j | jq --arg ws "$WORKSPACE" '[.[] | select(.workspace.id == ($ws | tonumber)) | select(.class | test("zen"; "i"))] | length')
 
-# Toggle floating, resize to 50% x 50% of screen, and center it
-hyprctl dispatch togglefloating "address:$ADDRESS" || \
-  exit 0  # exit silently if window no longer exists
-
-hyprctl dispatch resizewindowpixel exact 50% 50%,"address:$ADDRESS"
-hyprctl dispatch centerwindow "address:$ADDRESS"
+if [ "$COUNT" -eq 1 ]; then
+    hyprctl dispatch settiled "address:$ADDRESS"
+fi
