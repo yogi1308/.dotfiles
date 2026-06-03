5d9d4fe07258a8cd8dfab39e30c72f66c07c9c05
yogi1308 <shreetej.yogi@gmail.com>
2026-06-02 21:55:28 -0700

    misc changes

--- scripts/.config/scripts/show-desktop.sh
+++ scripts/.config/scripts/show-desktop.sh
@@ -1,37 +1,24 @@
 #!/bin/env sh
-
 TMP_FILE="$XDG_RUNTIME_DIR/hyprland-show-desktop"
+CURRENT_WORKSPACE=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true) | .activeWorkspace | .name')
+CMDS=""
+TMP_ADDRESS=""
 
-CURRENT_WORKSPACE=$(hyprctl monitors -j | jq '.[] | .activeWorkspace | .name' | sed 's/"//g')
 
 if [ -s "$TMP_FILE-$CURRENT_WORKSPACE" ]; then
-  readarray -d $'\n' -t ADDRESS_ARRAY <<< $(< "$TMP_FILE-$CURRENT_WORKSPACE")
-
-  for address in "${ADDRESS_ARRAY[@]}"
-  do
-    CMDS+="dispatch movetoworkspacesilent name:$CURRENT_WORKSPACE,address:$address;"
-  done
-
+  while IFS= read -r address; do
+    [[ -n "$address" ]] && CMDS+="dispatch movetoworkspacesilent name:$CURRENT_WORKSPACE,address:$address;"
+  done < "$TMP_FILE-$CURRENT_WORKSPACE"
   hyprctl --batch "$CMDS"
-
   rm "$TMP_FILE-$CURRENT_WORKSPACE"
 else
-  HIDDEN_WINDOWS=$(hyprctl clients -j | jq --arg CW "$CURRENT_WORKSPACE" '.[] | select (.workspace .name == $CW) | .address')
-
-  readarray -d $'\n' -t ADDRESS_ARRAY <<< $HIDDEN_WINDOWS
-
-  for address in "${ADDRESS_ARRAY[@]}"
-  do
-    address=$(sed 's/"//g' <<< $address )
-
-    if [[ -n address ]]; then
-      TMP_ADDRESS+="$address\n"
-    fi
-
-    CMDS+="dispatch movetoworkspacesilent special:desktop,address:$address;"
-  done
-
+  HIDDEN_WINDOWS=$(hyprctl clients -j | jq -r --arg CW "$CURRENT_WORKSPACE" '.[] | select(.workspace.name == $CW) | .address')
+  while IFS= read -r address; do
+    [[ -n "$address" ]] && {
+      TMP_ADDRESS+="$address"$'\n'
+      CMDS+="dispatch movetoworkspacesilent special:desktop,address:$address;"
+    }
+  done <<< "$HIDDEN_WINDOWS"
   hyprctl --batch "$CMDS"
-
-  echo -e "$TMP_ADDRESS" | sed -e '/^$/d' > "$TMP_FILE-$CURRENT_WORKSPACE"
+  printf '%s' "$TMP_ADDRESS" > "$TMP_FILE-$CURRENT_WORKSPACE"
 fi
